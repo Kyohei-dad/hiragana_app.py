e774916862b24e0f656aad99940fb851a8cf0c26
Kyohei-dad <inoopy69@gmail.com>
2025-03-27 12:13:35 +0900

    Update hiragana_app.py

--- hiraganaApp_project/hiragana_app.py
+++ hiraganaApp_project/hiragana_app.py
@@ -57,7 +57,7 @@ def set_background(image_path):
                 bottom: 90px;
             }}
             .quiz-box {{
-                font-size: 80px;
+                font-size: 60px;
             }}
             .choices-container {{
                 flex-direction: column;
